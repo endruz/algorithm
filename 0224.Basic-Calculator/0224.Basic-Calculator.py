4f9b70fa4a85f1dd17443d8be63dcb4dc8f43d49
endRuz <endruz@foxmail.com>
2021-03-10 20:49:41 +0800

    add 0224

---
+++ 0224.Basic-Calculator/0224.Basic-Calculator.py
@@ -0,0 +1,45 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = list()
+        index = 0
+        length = len(s)
+
+        def cal(s):
+            l = len(s)
+            num1 = 0
+            num2 = ""
+            operator = "+"
+            for i in range(l):
+                if s[i].isdigit():
+                    num2 += s[i]
+                    if i == l - 1 or s[i+1] in ["+", "-", " "]:
+                        if operator == "+":
+                            num1 = num1 + int(num2)
+                        elif operator == "-":
+                            num1 = num1 - int(num2)
+                        num2 = ""
+                elif s[i] in ["+", "-"]:
+                    if i > 0 and s[i-1] == "-":
+                        operator = "+" if s[i] == "-" else "-"
+                    else:
+                        operator = s[i]
+            return num1
+
+
+        while index < length:
+            if s[index] == '(':
+                stack.append(index)
+                index += 1
+            elif s[index] == ')':
+                start = stack.pop()
+                value = str(cal(s[start+1: index]))
+                if index == length - 1:
+                    s = s[:start] + value
+                else:
+                    s = s[:start] + value + s[index+1:]
+                length = len(s)
+                index = start + len(value) - 1
+            else:
+                index += 1
+
+        return int(cal(s))

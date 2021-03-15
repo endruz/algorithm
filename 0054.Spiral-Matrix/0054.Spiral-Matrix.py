c61af63bdafc684aec51f4f6fa3cde0e95256718
endRuz <endruz@foxmail.com>
2021-03-15 11:49:17 +0800

    add 0054

---
+++ 0054.Spiral-Matrix/0054.Spiral-Matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = matrix[0]
+        operator = -1
+        del matrix[0]
+
+        def fun(matrix, operator):
+            length = len(matrix)
+            if operator == -1:
+                start = 0
+                stop = length
+                step = 1
+            else:
+                start = length - 1
+                stop = -1
+                step = -1
+            for i in range(start, stop, step):
+                result.append(matrix[i].pop(operator))
+            while matrix[operator]:
+                result.append(matrix[operator].pop(operator))
+            for index in range(length-1, -1, -1):
+                if not matrix[index]:
+                    del matrix[index]
+
+        while matrix:
+            fun(matrix, operator)
+            operator = -1 if operator == 0 else 0
+
+        return result

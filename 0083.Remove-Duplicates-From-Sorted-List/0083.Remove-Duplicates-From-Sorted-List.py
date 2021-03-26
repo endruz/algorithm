0d63939075f9c7363c641164f7c9d09775f7f419
endRuz <endruz@foxmail.com>
2021-03-26 09:30:40 +0800

    add 0083

---
+++ 0083.Remove-Duplicates-From-Sorted-List/0083.Remove-Duplicates-From-Sorted-List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        '''
+        新建 pointNode 节点指向 head，防止 head 为 None。返回时直接返回 pointNode.next 即可。
+        从 pointNode 开始对链表进行遍历。
+        如果当前 currNode 与 currNode.next 值相同，则将 currNode 指向 currNode.next.next。
+        '''
+        pointNode = ListNode(None, head)
+        currNode = pointNode
+        while currNode.next:
+            if currNode.val == currNode.next.val:
+                currNode.next = currNode.next.next
+            else:
+                currNode = currNode.next
+        return pointNode.next

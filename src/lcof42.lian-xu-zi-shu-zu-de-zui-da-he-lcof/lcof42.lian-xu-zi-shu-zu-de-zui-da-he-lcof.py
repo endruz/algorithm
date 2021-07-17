6aecc250937f814f8fb9f522b322956035ef1b02
endruz <endruz@foxmail.com>
2021-07-17 11:43:32 +0800

    add divide and conquer algorithm to lcof42

--- src/lcof42.lian-xu-zi-shu-zu-de-zui-da-he-lcof/lcof42.lian-xu-zi-shu-zu-de-zui-da-he-lcof.py
+++ src/lcof42.lian-xu-zi-shu-zu-de-zui-da-he-lcof/lcof42.lian-xu-zi-shu-zu-de-zui-da-he-lcof.py
@@ -27,3 +27,33 @@ class Solution2:
             pre = max(pre + num, num)
             result = max(result, pre)
         return result
+
+
+class Solution3:
+    """
+    分治
+    """
+    class Status:
+        def __init__(self, lSum: int, rSum: int, mSum: int, iSum: int):
+            self.lSum = lSum # lSum 表示 [l,r] 内以 ll 为左端点的最大子段和
+            self.rSum = rSum # rSum 表示 [l,r] 内以 rr 为右端点的最大子段和
+            self.mSum = mSum # mSum 表示 [l,r] 内的最大子段和
+            self.iSum = iSum # iSum 表示 [l,r] 的区间和
+
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self.getInfo(nums, 0, len(nums) - 1).mSum
+
+    def getInfo(self, arr: List[int], l: int, r: int) -> Status:
+        if l == r:
+            return self.Status(arr[l], arr[l], arr[l], arr[l])
+        m = (l + r) // 2
+        lStatus = self.getInfo(arr, l, m)
+        rStatus = self.getInfo(arr, m + 1, r)
+        return self.pushUp(lStatus, rStatus)
+
+    def pushUp(self, lStatus: Status, rStatus: Status) -> Status:
+        iSum = lStatus.iSum + rStatus.iSum
+        lSum = max(lStatus.lSum, lStatus.iSum + rStatus.lSum)
+        rSum = max(rStatus.rSum, rStatus.iSum + lStatus.rSum)
+        mSum = max(lStatus.mSum, rStatus.mSum, lStatus.rSum + rStatus.lSum)
+        return self.Status(lSum, rSum, mSum, iSum)

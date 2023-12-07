1f7549fd4a2f1a7deccb96932b27bd4082746265
endruz <endruz@foxmail.com>
2023-12-07 15:04:44 +0800

    add advent-of-code/2023/Day7

---
+++ advent-of-code/2023/Day7/day7_1.py
@@ -0,0 +1,59 @@
+# https://adventofcode.com/2023/day/7
+# --- Part One ---
+
+from collections import Counter
+
+label_list = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+label_map = {v: i for i, v in enumerate(label_list)}
+
+hands_list: list[list[tuple[str, int]]] = [[] for _ in range(7)]
+
+
+def get_type_index(hands: str) -> int:
+    counter = sorted(Counter(hands).values(), reverse=True)
+    if counter[0] == 5:
+        return 6
+    if counter[0] == 4:
+        return 5
+    if counter[0] == 3:
+        if counter[1] == 2:
+            return 4
+        else:
+            return 3
+    if counter[0] == 2:
+        if counter[1] == 2:
+            return 2
+        else:
+            return 1
+    return 0
+
+
+def cmp(hands1: str, hands2: str) -> int:
+    for index in range(len(hands1)):
+        v1 = label_map[hands1[index]]
+        v2 = label_map[hands2[index]]
+        if v1 > v2:
+            return 1
+        if v1 < v2:
+            return -1
+    return 0
+
+
+with open("./puzzle_input.txt", "r") as f:
+    for line in f.readlines():
+        hands, bid = line.strip().split()
+        for index, hands_bid_pair in enumerate(hands_list[get_type_index(hands)]):
+            if cmp(hands_bid_pair[0], hands) >= 0:
+                hands_list[get_type_index(hands)].insert(index, (hands, int(bid)))
+                break
+        else:
+            hands_list[get_type_index(hands)].append((hands, int(bid)))
+
+result = rank = 0
+for h in hands_list:
+    for hands_bid_pair in h:
+        rank += 1
+        winnings = hands_bid_pair[1] * rank
+        result += winnings
+
+print(result)
